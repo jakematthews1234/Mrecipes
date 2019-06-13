5a2f00a46c8ad5012f2fd585211e5a3c261f2d7c
jakematthews1234 <jakematthews072@gmail.com>
2019-06-13 15:16:11 +0100

    created an add recipe function on the front end

--- Mrecipes.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-MONGO_URI= mongodb+srv://jake123:jake123@recipebook-om5hb.mongodb.net/Mrecipes?retryWrites=true
--- app.py
+++ app.py
@@ -3,18 +3,24 @@ from flask import Flask, render_template, redirect, request, url_for
 from pymongo import MongoClient
 from bson.objectid import ObjectId
 
-#connect to mongodb
+# connect to mongodb
 app = Flask(__name__)
 client = MongoClient(os.environ.get('MONGO_URI'))
 db = client.recipebook
 
 
 @app.route('/')
-#returning the index.html template
+# returning the index.html template
 def index():
     recipes = [recipe for recipe in db.recipes.find({})]
     return render_template('index.html', recipes=recipes)
 
-#setting up flask
+# adding a add recipe route to my web page.
+@app.route('/addrecipe')
+def add_recipe():
+    return render_template('addrecipe.html')
+
+
+# setting up flask
 if __name__ == '__main__':
     app.run(host=os.getenv('IP'), port=os.getenv('PORT'), debug=True)
